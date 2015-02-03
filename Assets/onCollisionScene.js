c043bc525b10eff3253b926c633764a16c1bdb38
hicks <oconshien@gmail.com>
2015-02-03 08:43:48 -0500

    -window

--- Assets/onCollisionScene.js
+++ Assets/onCollisionScene.js
@@ -2,7 +2,12 @@
 
 function OnTriggerEnter (col : Collider)
 {
-    if(col.gameObject.name == "Staircase_Up")
+    if(col.gameObject.name == "Staircase_window")
+    {
+        Application.LoadLevel ("hallway");
+    }
+    
+    if(col.gameObject.name == "lock_door-r")
     {
         Application.LoadLevel ("hallway");
     }
