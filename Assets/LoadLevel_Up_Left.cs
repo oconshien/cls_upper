92a55918e69ed820aff9d91dde938e3fa86e140f
tidwell <ctidwelltc@gmail.com>
2015-02-23 08:51:29 -0500

    working on the auto fade script

---
+++ Assets/LoadLevel_Up_Left.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel_Up_Left : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		AutoFade.LoadLevel ("2", 2, 4, Color.black);
+	}
+}
