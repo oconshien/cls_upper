cb2e1533fd05f9c7ffe0427a5826f50d860f757a
tidwell <ctidwelltc@gmail.com>
2015-02-25 08:52:02 -0500

    set the time for fade in and fade out to 1 sec

--- Assets/Scripts/LoadLevel_Up_Left.cs
+++ Assets/Scripts/LoadLevel_Up_Left.cs
@@ -6,6 +6,6 @@ public class LoadLevel_Up_Left : MonoBehaviour
 
 	void OnTriggerEnter(Collider other)
 	{
-		AutoFade.LoadLevel ("2", 2, 4, Color.black);
+		AutoFade.LoadLevel ("2", 1, 1, Color.black);
 	}
 }
