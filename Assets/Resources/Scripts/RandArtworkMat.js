79ef1bd5ddd8816a916d06810d29b8dec9a58d04
hicks <oconshien@gmail.com>
2015-03-10 08:28:00 -0400

    -ovr in the works

--- Assets/Resources/Scripts/RandArtworkMat.js
+++ Assets/Resources/Scripts/RandArtworkMat.js
@@ -1,16 +1,17 @@
-﻿#pragma strict
+﻿/*
+#pragma strict
  
 public class RandArtworkMat : MonoBehaviour {
  
 	var textures: Texture2D[]; // assign some textures here in the Inspector
  
-/* 	function Start(){
+ 	function Start(){
     	while (true){
 			renderer.material.SetTexture ("_Art", textures[Random.Range(0, textures.Length)]);
 			yield WaitForSeconds(1.0);
     	}
 	}
-*/	
+	
 	// Assigns a texture named "Assets/Resources/glass" to a Plane.
 	function Start () {
 		var go = GameObject.Find("_Art");
@@ -18,3 +19,5 @@ public class RandArtworkMat : MonoBehaviour {
 		rend.material.SetTexture ("_Art", textures[Random.Range(0, textures.Length)]);
 	}
 }
+
+*/
